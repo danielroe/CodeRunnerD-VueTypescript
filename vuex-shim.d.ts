770db5cfcfa8f4aeb216876c9b2bde7a42db382e
Daniel Roe <daniel@roe.dev>
2024-08-20 11:55:26 +0100

    fix: augment `vue` rather than `@vue/runtime-core`

--- vuex-shim.d.ts
+++ vuex-shim.d.ts
@@ -2,7 +2,7 @@ import { ComponentCustomProperties } from 'vue'
 import { Store } from 'vuex'
 import { State } from './src/store/global.store'
 
-declare module '@vue/runtime-core' {
+declare module 'vue' {
   interface ComponentCustomProperties {
     $store: Store<State>
   }
